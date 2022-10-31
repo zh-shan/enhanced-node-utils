32f0fe9019fe97e87252dd8feeb3f753b080c1f4
haishan.zhang <haishan.zhang@southgis.com>
2022-10-31 15:01:23 +0800

    build: 使用 rollup 进行编译

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import getPackageJson from './get-package-json'
+import getRoot from './get-root'
+import packageExists from './package-exists'
+import safeRequire from './safe-require'
+
+export default {
+  getPackageJson,
+  getRoot,
+  packageExists,
+  safeRequire
+}
